6e7557d8c76e26cb2ca27b037e2567f80ce035ef
Tppv <vieira.tiago@scalapartner.com.br>
2024-07-11 23:49:47 -0300

    initial commit to the Stone project - challenge

---
+++ stone_desafio_backend/src/main/java/br/com/tiagoppvieira/stone_desafio_backend/StoneDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagoppvieira.stone_desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneDesafioBackendApplication.class, args);
+	}
+
+}
